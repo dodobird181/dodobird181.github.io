2d96392c81615f4dcf932d37fd11b1f5f2dfdcde
Samuel Morris <dodobird181@gmail.com>
2021-01-19 16:41:39 -0500

    Update captcha.js

--- contact/captcha.js
+++ contact/captcha.js
@@ -1,7 +1,7 @@
-let cap = document.getElementById("myCaptcha")
+
+
+
 let submitButton = document.getElementById("submitButton")
 setInterval(() => {
-    if(cap.getResponse() == ""){
-        submitButton.style.display = "block"
-    }
+    console.log(grecaptcha.getResponse())
 }, 2000)
