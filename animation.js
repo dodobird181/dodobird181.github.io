2e2ebe12bc9ac1dfc513f95ce15d0dad37fa1b25
Samuel Morris <dodobird181@gmail.com>
2021-01-20 11:30:28 -0500

    Animation update (Shitty tracking of mouse movements that needs improvements)

--- animation.js
+++ animation.js
@@ -1,8 +1,62 @@
 
-const DX = 650
-const DY = 200
+
+const TIME_MULT = 750
+const TIME_BASE = 500
+
+let body = document.getElementById("body")
+let centerX = document.body.style.width - (document.body.style.width/2)
+let centerY = document.body.style.height - (document.body.style.height/2)
+let DX = 0
+let DY = 0
+body.onmousemove = (e) => {
+        DX = (centerX - e.clientX) * -1
+        DY = (centerY - e.clientY) * -1 / 2
+        console.log(DX + ", " + DY)
+}
 
 let scaleRange = [1, 2]
+
+var timeline = anime.timeline({
+    loop: false,
+    easing: 'easeInOutSine'
+});
+timeline.add({
+    targets: ".welcome",
+    opacity: 1,
+    duration: 1500
+})
+timeline.complete = () => {
+    startAnimatingShapes()
+}
+
+function startAnimatingShapes(){
+    giveSize(25, ".rect", 1)
+    giveSize(50, ".rect", 2)
+    giveSize(75, ".rect", 3)
+    giveSize(25, ".ball", 1)
+    giveSize(50, ".ball", 2)
+
+    // Trigger the animation loops for each shape seperately
+    const NUM_RECTS = 4
+    for(var i = 1; i <= NUM_RECTS; i++){
+        refreshAnimationRect(i)
+    }
+    const NUM_BALLS = 4
+    for(var i = 1; i <= NUM_BALLS; i++){
+        refreshAnimationBall(i)
+    }
+}
+
+// Used to slowly increase the size of a shape to it's default size
+function giveSize(size, shapeString, shapeNum){
+    anime({
+        targets: shapeString + shapeNum,
+        width: size + 'px',
+        height: size + 'px',
+        duration: 5000
+    })
+}
+
 function refreshAnimationRect(num) {
     var timeline = anime.timeline({
         loop: false,
@@ -11,19 +65,12 @@ function refreshAnimationRect(num) {
     });
     timeline
         .add({
-            translateX: ((Math.random() - 0.5) * DX) + 'px',
-            translateY: ((Math.random() - 0.5) * DY) + 'px',
+            translateX: ((Math.random()) * DX) + 'px',
+            translateY: ((Math.random()) * DY) + 'px',
             scale: getRealFrom(scaleRange[0], scaleRange[1]),
             rotate: Math.random() * 180,
-            duration: (Math.random() * 750) + 1000
+            duration: (Math.random() * TIME_MULT) + TIME_BASE
         })
-        .add({
-            translateX: ((Math.random() - 0.5) * DX) + 'px',
-            translateY: ((Math.random() - 0.5) * DY) + 'px',
-            scale: getRealFrom(scaleRange[0], scaleRange[1]),
-            rotate: Math.random() * 180,
-            duration: (Math.random() * 750) + 1000
-        });
     timeline.complete = function() {
         refreshAnimationRect(num);
     };
@@ -37,32 +84,15 @@ function refreshAnimationBall(num) {
     });
     timeline
         .add({
-            translateX: ((Math.random() - 0.5) * DX) + 'px',
-            translateY: ((Math.random() - 0.5) * DY) + 'px',
+            translateX: ((Math.random()) * DX) + 'px',
+            translateY: ((Math.random()) * DY) + 'px',
             scale: getRealFrom(scaleRange[0], scaleRange[1]),
             rotate: Math.random() * 180,
-            duration: (Math.random() * 750) + 1000
+            duration: (Math.random() * TIME_MULT) + TIME_BASE
         })
-        .add({
-            translateX: ((Math.random() - 0.5) * DX) + 'px',
-            translateY: ((Math.random() - 0.5) * DY) + 'px',
-            scale: getRealFrom(scaleRange[0], scaleRange[1]),
-            rotate: Math.random() * 180,
-            duration: (Math.random() * 750) + 1000
-        });
     timeline.complete = function() {
         refreshAnimationBall(num);
     };
-}
-
-// Trigger the animation loops for each shape seperately
-const NUM_RECTS = 4
-for(var i = 1; i <= NUM_RECTS; i++){
-    refreshAnimationRect(i)
-}
-const NUM_BALLS = 4
-for(var i = 1; i <= NUM_BALLS; i++){
-    refreshAnimationBall(i)
 }
 
 /**
