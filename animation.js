8d83693af4c022117668705bdf9b2f660b051d7e
Samuel Morris <dodobird181@gmail.com>
2021-01-28 11:07:00 -0500

    Better animation on welcome page!

--- animation.js
+++ animation.js
@@ -1,73 +1,35 @@
-const TIME_MULT = 500
-const TIME_BASE = 500
 
-let DX = 0
-let DY = 0
+const TIME_MULT = 1000
+const TIME_BASE = 1000
 
-body.onmousemove = (e) => {
-    let page = document.documentElement
-    let centerX = page.clientWidth - (page.clientWidth/2)
-    let centerY = page.clientHeight - (page.clientHeight/2)
-    console.log("centerX: " + centerX)
-    DX = (centerX - e.clientX) * -1
-    DY = (centerY - e.clientY) * -1 / 2
-    console.log(DX + ", " + DY)
+//Randomly choose hue for the animated rectangles!
+
+colorify(getRealFrom(0, 360))
+
+function colorify(hue){
+    let r1 = document.getElementById("rect1")
+    let r2 = document.getElementById("rect2")
+    let r3 = document.getElementById("rect3")
+    let col1 = "hsl(" + hue + ", 100%, 75%)"
+    let col2 = "hsl(" + hue + ", 100%, 50%)"
+    let col3 = "hsl(" + hue + ", 100%, 15%)"
+    r1.style.backgroundColor = col1
+    r2.style.backgroundColor = col2
+    r3.style.backgroundColor = col3
 }
 
-let scaleRange = [1, 2]
-
-var timeline = anime.timeline({
-    loop: false,
-    easing: 'easeInOutSine'
-});
-timeline.add({
-    targets: ".welcome",
-    opacity: 1,
-    duration: 1500
-})
-timeline.complete = () => {
-    startAnimatingShapes()
-}
-
-function startAnimatingShapes(){
-    giveSize(25, ".rect", 1)
-    giveSize(50, ".rect", 2)
-    giveSize(75, ".rect", 3)
-    giveSize(25, ".ball", 1)
-    giveSize(50, ".ball", 2)
-
-    // Trigger the animation loops for each shape seperately
-    const NUM_RECTS = 4
-    for(var i = 1; i <= NUM_RECTS; i++){
-        refreshAnimationRect(i)
-    }
-    const NUM_BALLS = 4
-    for(var i = 1; i <= NUM_BALLS; i++){
-        refreshAnimationBall(i)
-    }
-}
-
-// Used to slowly increase the size of a shape to it's default size
-function giveSize(size, shapeString, shapeNum){
-    anime({
-        targets: shapeString + shapeNum,
-        width: size + 'px',
-        height: size + 'px',
-        duration: 5000
-    })
-}
-
+refreshAnimationRect(1)
+refreshAnimationRect(2)
+refreshAnimationRect(3)
 function refreshAnimationRect(num) {
     var timeline = anime.timeline({
         loop: false,
         targets: ".rect" + num,
-        easing: 'easeInOutSine'
+        easing: 'linear'
     });
     timeline
         .add({
-            translateX: ((Math.random()) * DX) + 'px',
-            translateY: ((Math.random()) * DY) + 'px',
-            scale: getRealFrom(scaleRange[0], scaleRange[1]),
+            scale: getRealFrom(1.5, 2),
             rotate: Math.random() * 180,
             duration: (Math.random() * TIME_MULT) + TIME_BASE
         })
@@ -76,27 +38,8 @@ function refreshAnimationRect(num) {
     };
 }
 
-function refreshAnimationBall(num) {
-    var timeline = anime.timeline({
-        loop: false,
-        targets: ".ball" + num,
-        easing: 'easeInOutSine'
-    });
-    timeline
-        .add({
-            translateX: ((Math.random()) * DX) + 'px',
-            translateY: ((Math.random()) * DY) + 'px',
-            scale: getRealFrom(scaleRange[0], scaleRange[1]),
-            rotate: Math.random() * 180,
-            duration: (Math.random() * TIME_MULT) + TIME_BASE
-        })
-    timeline.complete = function() {
-        refreshAnimationBall(num);
-    };
-}
-
 /**
- * Returns a real number in the range from a to b.
+ * Returns a real number in the rage from a to b.
  * Condition: a <= b.
  * @param {*} a smaller than b
  * @param {*} b larger than a.
